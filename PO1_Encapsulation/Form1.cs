458748dfeb602aa3d1fd37dcd2a83bee39164b16
MioraGaelle <ranaivomioragaelle@gmail.com>
2024-02-08 11:42:18 -0500

    chgt 1

--- PO1_Encapsulation/Form1.cs
+++ PO1_Encapsulation/Form1.cs
@@ -2,24 +2,61 @@ namespace PO1_Encapsulation
 {
     public partial class Form1 : Form
     {
+        Personnage joueur = new Personnage();
+        Personnage dragon = new();
+
         public Form1()
         {
             InitializeComponent();
+            //Personnage personnage = new Personnage(); //acceder au personnage
+            //personnage.PointsDePuissance = 5;
+            //personnage.PointsDeVie = -10;
+            //personnage.Nom = "Flash";
+            //MessageBox.Show(personnage.PointsDeVie.ToString());
+
+            //DateTime momentPresent = DateTime.Now;
+            //momentPresent.Year = 1; // on ne peut pas modidier momentPresent, c'est en lecture seule
+
+            //if (personnage.EstEnVie)
+            //{
+            //    MessageBox.Show("Vous êtes en vie.");
+            //}
+
+            joueur.PointsDeVie = 10;
+            joueur.PointsDePuissance = 1;
+
+            dragon.PointsDeVie = joueur.PointsDeVie * 3;
+            dragon.PointsDePuissance = 3;
+        }
+
+        private void ActualiserLabels()
+        {
+            lbl_pointsVieJoueur.Text = "Vie du joueur : " + joueur.PointsDeVie.ToString();
+            lbl_puissanceAttaqueJoueur.Text = "Puissance d'attaque du joueur : " + joueur.PointsDePuissance.ToString();
+
+            lbl_pointsVieDragon.Text = "Vie du dragon: " + dragon.PointsDeVie.ToString();
+            lbl_puissanceAttaqueDragon.Text = "Puissance d'attaque du dragon: " + dragon.PointsDePuissance.ToString();
         }
 
         private void btn_attaquer_Click(object sender, EventArgs e)
         {
-
+            dragon.PointsDeVie -= joueur.PointsDePuissance;
+            ActualiserLabels();
         }
 
         private void btn_seSoigner_Click(object sender, EventArgs e)
         {
-
+            ActualiserLabels();
         }
 
         private void btn_puissanceAttaque_Click(object sender, EventArgs e)
         {
+            ActualiserLabels();
+        }
 
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            ActualiserLabels();
         }
     }
 }
---
+++ PO1_Encapsulation/Personnage.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PO1_Encapsulation
+{
+    public class Personnage //toujours mettre : public
+    {
+        string nom;
+        int pointsDeVie;
+        int pointsDePuissance;
+        // Pour protéger les attributs //selectionner les 3 lignes en haut
+        //click droite //Action rapides et refactorisation 
+        //Encapsuler les champs (et utiliser la propriétés)
+
+        public string Nom { get => nom; set => nom = value; }
+        public int PointsDeVie { get => pointsDeVie;
+            set
+            {
+                if(value < 0)
+                {
+                    pointsDeVie = 0;
+                }
+                else
+                {
+                    pointsDeVie = value;
+                }
+            }
+        }
+        public int PointsDePuissance { get => pointsDePuissance; set => pointsDePuissance = value; }
+
+        public bool EstEnVie //boolean parce que retourne vrai ou faux 
+            //EstEnVie est une propriété 
+        {
+            get 
+            {
+                if (PointsDeVie > 0)
+                {
+                    return true;
+                }
+                return false;
+            }
+        }
+    }
+}
